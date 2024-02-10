c51880dcc14668bd319d6c330a2963f79afa6486
Jyoti312004 <jyotichander79@gmail.com>
2024-02-10 14:36:56 +0530

    updated design doc

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,12 +4,8 @@ import { verify } from "../midlleware/auth.js";
 
 const router = express.Router();
 
-/* READ */
 router.get("/", verify, getFeedPosts);
-
 router.get("/:userId/posts", verify, getUserPosts);
-
-/* UPDATE */
 router.patch("/:id/like", verify, likePost);
 
 export default router;
