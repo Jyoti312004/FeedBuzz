dac931f8ed500d7831c08ae737e3839adf1f9326
Jyoti312004 <jyotichander79@gmail.com>
2024-03-02 10:40:29 +0530

    updating files

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          Sociopedia
+          FeedBuzz
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
